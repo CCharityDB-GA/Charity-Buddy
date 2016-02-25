06ebe203fcd7ef12f5d1f989b110d8966b31afcc
Charles <codprestige10@gmail.com>
2016-02-25 16:52:34 -0500

    got sign up with devise working, also created secure user table

--- charity-buddy/config/routes.rb
+++ charity-buddy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  #devise_for :users
 
   root to: "welcome#index"
 
